f5dd3dd1477954ff703bc279d2c88de72b4cd431
Aleksandr <alexwebua1@gmail.com>
2021-07-20 22:45:04 +0300

    Expanded ControlPanel functionality

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -1,21 +1,79 @@
 import React from 'react';
 
+class Control extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isActive: this.props.isActive ?? false,
+        }
+    }
+
+    render() {
+        const title = this.props.title;
+
+        return(
+            <div className={`control ${this.state.isActive ? 'active' : ''}`} onClick={this.props.onClick}>
+                {title}
+            </div>
+        );
+    }
+}
+
+class PrioritySelectorControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.defaultValue
+        }
+
+        this.options = [];
+        this.props.options.map(option => {
+            this.options.push(
+                <option value={option.value} key={option.value}>
+                    {option.title}
+                </option>
+            );
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({value: e.target.value});
+    }
+
+    render() {
+        return (
+            <label className='control priority-selector'>
+                Priority:
+                <select id="prioritySelector" value={this.state.value} onChange={this.handleChange}>
+                    {this.options}
+                </select>
+            </label>
+        );
+    }
+}
+
 class ControlPanel extends React.Component {
+    handleClick = (e) => {
+        //TODO: change active control, move it to parent component, change view of the list
+        console.log(e.target);
+    }
+
     render() {
         return (
             <div className='list__controls'>
-                <div className='control active'>All</div>
-                <div className='control'>Active</div>
-                <div className='control'>Done</div>
-                <label className='control priority-selector'>
-                    Priority:
-                    <select id="prioritySelector" defaultValue={'all'}>
-                        <option value="all">All</option>
-                        <option value="high">High</option>
-                        <option value="medium">Medium</option>
-                        <option value="low">Low</option>
-                    </select>
-                </label>
+                <Control title='All' onClick={this.handleClick} isActive={true} />
+                <Control title='Active' onClick={this.handleClick} />
+                <Control title='Done' onClick={this.handleClick} />
+                <PrioritySelectorControl
+                    options={[
+                        {value: 'all', title: 'All'},
+                        {value: 'high', title: 'High'},
+                        {value: 'medium', title: 'Medium'},
+                        {value: 'low', title: 'Low'}
+                    ]}
+                    defaultValue='all'
+                />
             </div>
         );
     }
